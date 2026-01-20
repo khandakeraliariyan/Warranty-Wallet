eee36f19013402cbfebdb8eeb3d5b036546bff3c
Khandaker Ali Ariyan <khandakeraliariyanndc@gmail.com>
2026-01-21 02:31:56 +0600

    MongoDB connected with backend

--- server/src/server.js
+++ server/src/server.js
@@ -1,7 +1,11 @@
 import dotenv from "dotenv";
 import app from "./app.js";
+import connectDB from "./config/db.js";
 
 dotenv.config();
+
+// Connect to MongoDB
+connectDB();
 
 const PORT = process.env.PORT || 5000;
 
